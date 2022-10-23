8647354dd1fcef946e7090954ce028eed9b26c55
Paulo Souza <paulo.souza@petzpartner.com.br>
2022-10-23 19:32:33 -0300

    refactor(grid): método principal dividido em vários métodos

--- src/main/java/dev/paulosouza/tcc/service/GridService.java
+++ src/main/java/dev/paulosouza/tcc/service/GridService.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class GridService {
@@ -41,12 +40,109 @@ public class GridService {
     }
 
     private Map<Integer, List<ClassroomResponse>> generate(List<Subject> pendingSubjects, GridRequest request) {
-        Map<Integer, List<ClassroomResponse>> groupByColor = new HashMap<>();
-        Map<Integer, List<ClassroomResponse>> grid = new HashMap<>();
-        Map<String, List<Classroom>> classroomMap = new HashMap<>();
+        Map<Integer, List<ClassroomResponse>> response = new HashMap<>();
+
+        // busca map de salas de aula
+        Map<String, List<Classroom>> classrooms = this.getClassrooms();
+
+        // ordenação decrescente pelo grau dos vértices
+        List<Subject> subjects = this.cloneAndSortDesc(pendingSubjects);
+
+        // coloração pelo algoritmo Welsh e Powell
+        this.colore(subjects);
+
+        // disciplinas atrasadas
+        List<Subject> lateSubjects  = this.getLateSubjects(request, subjects);
+        List<Classroom> lateClassrooms = this.getLateSubjectClassrooms(lateSubjects, classrooms);
+        response.put(0, ClassroomMapper.INSTANCE.toResponse(lateClassrooms));
+
+        int semester = request.getPeriod();
+
+        // itera até alocar todas as disciplinas
+        while (!subjects.isEmpty()) {
+            int finalSemester = semester;
+
+            // busca disciplinas do semestre atual
+            List<Subject> currentSubjects =
+                    getCurrentSemesterSubjects(request, classrooms, subjects, lateClassrooms, finalSemester);
+
+            // busca salas de aula do semestre atual
+            List<Classroom> currentClassrooms = getCurrentSemesterClassrooms(classrooms, currentSubjects);
+
+            response.put(semester - request.getPeriod(), ClassroomMapper.INSTANCE.toResponse(currentClassrooms));
+
+            // remove as disciplinas que já foram alocadas
+            subjects.removeAll(currentSubjects);
+
+            // incrementa para o próximo semestre
+            semester++;
+        }
+
+
+        return response;
+    }
+
+    private static List<Classroom> getCurrentSemesterClassrooms(Map<String, List<Classroom>> classroomMap, List<Subject> currentSemesterSubjects) {
+        return currentSemesterSubjects
+                .stream()
+                .flatMap(subject -> classroomMap.get(subject.getId()).stream())
+                .collect(Collectors.toList());
+    }
+
+    private static List<Subject> getCurrentSemesterSubjects(
+            GridRequest request,
+            Map<String, List<Classroom>> classroomMap,
+            List<Subject> subjects,
+            List<Classroom> lateSubjectsClassrooms,
+            int semester
+    ) {
+        return subjects.stream()
+                .filter(subject -> subject.getPeriod() <= semester)
+                .filter(subject -> {
+                    List<Classroom> subjectClassrooms = classroomMap.get(subject.getId());
+                    List<ClassroomTime> subjectTimes = new ArrayList<>();
+
+                    subjectClassrooms.forEach(sc -> subjectTimes.addAll(sc.getTimes()));
+
+                    List<ClassroomTime> lateTimes = lateSubjectsClassrooms.stream()
+                            .flatMap(classroom -> classroom.getTimes().stream())
+                            .collect(Collectors.toList());
+
+                    return semester != request.getPeriod() || Collections.disjoint(lateTimes, subjectTimes);
+                })
+                .collect(Collectors.toList());
+    }
+
+    private List<Subject> getLateSubjects(
+            GridRequest request,
+            List<Subject> subjects
+    ) {
+        List<Subject> lateSubjects = new ArrayList<>();
+        for (Subject subject : subjects) {
+            int period = subject.getPeriod();
+
+            if (period < request.getPeriod()) {
+                lateSubjects.add(subject);
+            }
+        }
+
+        subjects.removeAll(lateSubjects);
+
+        return lateSubjects;
+    }
+
+    private List<Classroom> getLateSubjectClassrooms(List<Subject> lateSubjects, Map<String, List<Classroom>> classroomMap) {
         List<Classroom> lateSubjectsClassrooms = new ArrayList<>();
 
+        lateSubjects.forEach(subject -> lateSubjectsClassrooms.addAll(classroomMap.get(subject.getId())));
+
+        return lateSubjectsClassrooms;
+    }
+
+    private Map<String, List<Classroom>> getClassrooms() {
         List<Classroom> classrooms = classroomRepository.findAll();
+
+        Map<String, List<Classroom>> classroomMap = new HashMap<>();
         classrooms.forEach(cr -> {
             List<Classroom> classroomList = classroomMap.get(cr.getSubjectId());
 
@@ -57,82 +153,10 @@ public class GridService {
             classroomMap.put(cr.getSubjectId(), classroomList);
         });
 
-        List<Subject> subjects = cloneAndSortDesc(pendingSubjects);
-
-        this.colore(subjects);
-
-        int maxColor = subjects.stream()
-                .map(Subject::getColor)
-                .max(Integer::compareTo)
-                .orElse(0);
-
-        for (int i = 0; i < maxColor + 1; ++i) {
-            int color = i;
-
-            List<Classroom> cr = subjects.stream()
-                    .filter(subject -> subject.getColor() == color)
-                    .map(Subject::getId)
-                    .flatMap(id -> classroomMap.get(id).stream())
-                    .collect(Collectors.toList());
-
-
-            groupByColor.put(i + 1, ClassroomMapper.INSTANCE.toResponse(cr));
-        }
-
-
-        List<Subject> lateSubjects = new ArrayList<>();
-
-        for (Subject subject : subjects) {
-            int period = subject.getPeriod();
-
-            if (period < request.getPeriod()) {
-                lateSubjects.add(subject);
-                lateSubjectsClassrooms.addAll(classroomMap.get(subject.getId()));
-            }
-        }
-        subjects.removeAll(lateSubjects);
-
-
-        grid.put(0, ClassroomMapper.INSTANCE.toResponse(lateSubjectsClassrooms));
-
-        int semester = request.getPeriod();
-
-        while (!subjects.isEmpty()) {
-            int finalSemester = semester;
-
-            List<Subject> currentSemesterSubjects = subjects.stream()
-                    .filter(subject -> subject.getPeriod() <= finalSemester)
-                    .filter(subject -> {
-                        List<Classroom> subjectClassrooms = classroomMap.get(subject.getId());
-                        List<ClassroomTime> subjectTimes = new ArrayList<>();
-
-                        subjectClassrooms.forEach(sc -> subjectTimes.addAll(sc.getTimes()));
-
-                        List<ClassroomTime> lateTimes = lateSubjectsClassrooms.stream()
-                                .flatMap(classroom -> classroom.getTimes().stream())
-                                .collect(Collectors.toList());
-
-                        return finalSemester != request.getPeriod() || Collections.disjoint(lateTimes, subjectTimes);
-                    })
-                    .collect(Collectors.toList());
-            List<Classroom> currentSemesterClassrooms = currentSemesterSubjects
-                    .stream()
-                    .flatMap(subject -> classroomMap.get(subject.getId()).stream())
-                    .collect(Collectors.toList());
-
-
-
-            grid.put(semester - request.getPeriod(), ClassroomMapper.INSTANCE.toResponse(currentSemesterClassrooms));
-            subjects.removeAll(currentSemesterSubjects);
-
-            semester++;
-        }
-
-
-        return grid;
+        return classroomMap;
     }
 
-    private static List<Subject> cloneAndSortDesc(List<Subject> pendingSubjects) {
+    private List<Subject> cloneAndSortDesc(List<Subject> pendingSubjects) {
         List<Subject> subjects = pendingSubjects.stream()
                 .map(Subject::clone)
                 .collect(Collectors.toList());
